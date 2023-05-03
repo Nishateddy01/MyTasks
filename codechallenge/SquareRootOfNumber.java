1534f8d08274e3c2a1686a12ac10c368638fa891
Nishateddy01 <132155320+Nishateddy01@users.noreply.github.com>
2023-05-03 21:52:39 +0530

    Add files via upload
    updated code

--- codechallenge/SquareRootOfNumber.java
+++ codechallenge/SquareRootOfNumber.java
@@ -4,14 +4,24 @@ public class SquareRootOfNumber {
 
 	public static void main(String[] args) {
 			
-		int no = 4;
+		int no = 8;
+		int i;
 		int sqrt = 1;
 
-	    for (int i = 1; i<=no; i++) 
+	    for ( i = 1; i<=no; i++) 
 	    {
 	        sqrt = sqrt - (sqrt * sqrt - no) / (2 * sqrt); //used square root formula
 	    }
+	    if(i*i==no) 
+	    {
+	    	
 	    System.out.println("Sqrt of a no: "+ sqrt);		
+	    }
+	    else // for i*i>no
+	    {
+		    System.out.println("Sqrt of a no: "+ (sqrt-1));		
+
+	    }
 		
 	}
 	}

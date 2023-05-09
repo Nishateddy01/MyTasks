b409752518ae67b8ff0bd8b3bc1f3424ed7f4d1e
Nishateddy01 <132155320+Nishateddy01@users.noreply.github.com>
2023-05-09 08:09:33 +0530

    Add files via upload
    Comiiting happyno

---
+++ codechallenge/HappyNumber.java
@@ -0,0 +1,35 @@
+package codechallenge;
+
+public class HappyNumber {
+	
+
+	public static void main(String[] args) {
+		int num = 82;
+		int sum = 0;
+		int temp;
+		 while(sum!=1 && sum!=4)
+		  {
+		   sum=0;
+		  
+		   while(num>0)
+		   {
+		    temp=num%10;
+		    sum+=(temp*temp);
+		    num=num/10;
+		   }
+		  
+		    num=sum;
+		  }
+								
+		if(sum == 1) {  			
+			System.out.println("The number entered is a Happy Number");
+        } else {
+            System.out.println(" The number entered is not a Happy Number");
+        }
+		
+		
+		
+
+	}
+	}
+
